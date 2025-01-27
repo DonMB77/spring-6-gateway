279ed4bd0345ddb89a15fb7f4b98c41ae4291045
DonMB77 <purposefuldrifter@gmail.com>
2025-01-27 13:33:08 +0100

    initial init

---
+++ src/test/java/com/drifter/spring_6_gateway/Spring6GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drifter.spring_6_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
