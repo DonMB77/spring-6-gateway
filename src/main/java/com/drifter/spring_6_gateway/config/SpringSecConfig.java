43827429f538674fa8c921a4bda8a12f4cace054
DonMB77 <purposefuldrifter@gmail.com>
2025-02-03 15:59:57 +0100

    added application-docker + app.yml indentation

--- src/main/java/com/drifter/spring_6_gateway/config/SpringSecConfig.java
+++ src/main/java/com/drifter/spring_6_gateway/config/SpringSecConfig.java
@@ -2,6 +2,7 @@ package com.drifter.spring_6_gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -13,8 +14,11 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 public class SpringSecConfig {
 
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        http.authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http){
+
+        http.authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec
+                        .pathMatchers("/oauth2/**", "/oauth2/token").permitAll()
+                        .anyExchange().authenticated())
                 .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()))
                 .csrf(ServerHttpSecurity.CsrfSpec::disable);
 
